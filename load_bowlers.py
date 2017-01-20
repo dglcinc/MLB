ddbc4044706612d6613254b3d7ac792b735f2a9e
David Lewis <david@dglc.com>
2017-01-20 07:06:01 -0500

    updated load script to make generic

--- load_bowlers.py
+++ load_bowlers.py
@@ -1,47 +1,22 @@
 from __future__ import print_function # Python 2/3 compatibility
-import boto3, random, csv, tqdm, time, botocore, uuid, json, decimal
+import boto3, random, csv, tqdm, time, botocore, uuid
+import json, decimal, collections, sys
 
+if len(sys.argv) != 3:
+	print("program to load a json file to a DynamoDB table")
+	print("usage: ", sys.argv[0], "file tablename")
+	sys.exit()
 db_r = boto3.resource('dynamodb')
+table = db_r.Table(sys.argv[2])
 
-table = db_r.Table('mlb_bowler')
+with open(sys.argv[1]) as json_file:
+    items = json.load(json_file,object_pairs_hook=collections.OrderedDict)
+    for item in items:
+	nb = {}
+        nb['uid'] = str(uuid.uuid1())
+	for attr in item:
+		nb[attr] = item[attr]
 
-with open("bowlers.json") as json_file:
-    bowlers = json.load(json_file, parse_float = decimal.Decimal)
-    for bowler in bowlers:
-        uid = uuid.uuid1()
-        firstname = bowler['firstname']
-        lastname = bowler['lastname']
-        clubmember = bowler['clubmember']
-        team = int(bowler['team'])
-        address = bowler['address']
-        city = bowler['city']
-        state = bowler['state']
-        zipcode = bowler['zip']
-        cellphone = bowler['cellphone']
-        homephone = bowler['homephone']
-        email = bowler['email']
-        altemail = bowler['altemail']
-        shirts = bowler['shirts']
-        roles = bowler['roles']
+        print("Adding to table(", sys.argv[2], ") item:", item)
 
-        print("Adding bowler:", firstname, lastname)
-        print("Adding bowler:", bowler)
-
-        table.put_item(
-           Item={
-               'fname': firstname,
-               'lname': lastname,
-               'member': clubmember,
-               'team': team,
-               'addr': address,
-               'city': city,
-               'st': state,
-               'zip': zipcode,
-               'cell': cellphone,
-               'homep': homephone,
-               'email': email,
-               'altm': altemail,
-               'shirts': shirts,
-               'roles': roles,
-            }
-        )
+#        table.put_item(nb)
