c5cbeeba451c2de98ec622fd4d1f85834bad7c16
David Lewis <david@dglc.com>
2017-01-20 08:19:46 -0500

    bowler table works

--- load_ddb.py
+++ load_ddb.py
@@ -2,21 +2,26 @@ from __future__ import print_function # Python 2/3 compatibility
 import boto3, random, csv, tqdm, time, botocore, uuid
 import json, decimal, collections, sys
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 5:
 	print("program to load a json file to a DynamoDB table")
-	print("usage: ", sys.argv[0], "file tablename")
+	print("usage: ", sys.argv[0], "file tablename gen-uid[true|false] parse-only[true|false]")
 	sys.exit()
+filename = sys.argv[1]
+gen_uid = sys.argv[3]
+parse_only = sys.argv[4]
 db_r = boto3.resource('dynamodb')
 table = db_r.Table(sys.argv[2])
 
-with open(sys.argv[1]) as json_file:
+with open(filename) as json_file:
     items = json.load(json_file,object_pairs_hook=collections.OrderedDict)
     for item in items:
-	nb = {}
-        nb['uid'] = str(uuid.uuid1())
+	nb = collections.OrderedDict({})
+        if gen_uid == 'true':
+		nb['uid'] = str(uuid.uuid1())
 	for attr in item:
 		nb[attr] = item[attr]
 
-        print("Adding to table(", sys.argv[2], ") item:", item)
+        print("Adding to table(", sys.argv[2], ") item:", nb)
 
-#        table.put_item(nb)
+	if parse_only == 'false':
+        	table.put_item(Item=nb)
